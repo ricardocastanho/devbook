26776effe101dbd7df6e280bf787496836c65942
ricardocastanho <ricardinhorl@hotmail.com>
2022-05-15 20:17:01 -0300

    ✨ creating users in controller

--- api/src/controller/users.go
+++ api/src/controller/users.go
@@ -1,23 +1,60 @@
 package controller
 
-import "net/http"
+import (
+	"api/src/config"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Get Users"))
+	w.Write([]byte("Get Users"))
 }
 
 func FindUser(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Find User"))
+	w.Write([]byte("Find User"))
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Create User"))
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := config.ConnectDatabase()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := repositories.NewUserRepo(db)
+
+	id, err := repo.CreateUser(&user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("User Created with ID: %s", id)))
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Update User"))
+	w.Write([]byte("Update User"))
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Delete User"))
+	w.Write([]byte("Delete User"))
 }
