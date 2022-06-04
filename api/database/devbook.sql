24446c81df9a9bce8aa997830b3516bebe47235f
ricardocastanho <48383095+ricardocastanho@users.noreply.github.com>
2022-06-04 13:09:31 -0300

    🗃️ update users table

--- api/database/devbook.sql
+++ api/database/devbook.sql
@@ -12,15 +12,15 @@ BEGIN
 END;;
 DELIMITER ;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE IF NOT EXISTS users (
-  id VARCHAR(255) NOT NULL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  username VARCHAR(50) NOT NULL UNIQUE,
-  password VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP DEFAULT current_timestamp(),
-  updated_at TIMESTAMP DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  deleted_at TIMESTAMP NULL DEFAULT NULL
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `username` VARCHAR(50) NOT NULL UNIQUE,
+  `password` VARCHAR(100) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT current_timestamp(),
+  `updated_at` TIMESTAMP DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` TIMESTAMP NULL DEFAULT NULL
 );
