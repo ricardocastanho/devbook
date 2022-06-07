bab752861016d02970682ed7e078df8418c12c1e
ricardocastanho <48383095+ricardocastanho@users.noreply.github.com>
2022-06-06 22:31:38 -0300

    💫 c

--- api/src/config/env.go
+++ api/src/config/env.go
@@ -8,13 +8,13 @@ import (
 )
 
 var (
-	DBName      = "devbook"
-	DBUser      = "devbook"
-	DBPassword  = "devbook"
-	DBHost      = "127.0.0.1"
-	DBPort      = "3300"
-	APIPort     = "4000"
-	APIJwtToken = "secret"
+	DBName          = "devbook"
+	DBUser          = "devbook"
+	DBPassword      = "devbook"
+	DBHost          = "127.0.0.1"
+	DBPort          = "3300"
+	APIPort         = "4000"
+	APIJWTSignature = "secret"
 )
 
 func LoadEnv() {
@@ -28,5 +28,5 @@ func LoadEnv() {
 	DBUser = os.Getenv("DB_USER")
 	DBPassword = os.Getenv("DB_PASSWORD")
 	APIPort = os.Getenv("API_PORT")
-	APIJwtToken = os.Getenv("API_JWT_TOKEN=secret")
+	APIJWTSignature = os.Getenv("API_JWT_TOKEN=secret")
 }
