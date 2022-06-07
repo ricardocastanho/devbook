98295e2e6d66232fffc6a03ced323b3eb927be47
ricardocastanho <48383095+ricardocastanho@users.noreply.github.com>
2022-06-06 22:32:15 -0300

    🛂 add GenerateToken support function

--- api/src/support/auth.go
+++ api/src/support/auth.go
@@ -1,6 +1,12 @@
 package support
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"api/src/config"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Hash(s string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
@@ -8,4 +14,15 @@ func Hash(s string) ([]byte, error) {
 
 func CompareHashAndPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func GenerateToken(userID string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = userID
+	claims["exp"] = time.Now().Add(time.Hour * 2).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(config.APIJWTSignature))
 }
