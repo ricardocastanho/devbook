08f339eb4609003f33755c9eb481976a6abae86d
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2022-07-19 23:45:13 -0300

    ✨ add posts controller

---
+++ api/src/controller/post.go
@@ -0,0 +1,68 @@
+package controller
+
+import (
+	"api/src/config"
+	"api/src/models"
+	"api/src/presenters"
+	"api/src/repositories"
+	"api/src/support"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func CreatePosts(w http.ResponseWriter, r *http.Request) {
+	userID, err := support.GetUserLoggedFromToken(r)
+
+	if err != nil {
+		presenters.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		presenters.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var post models.Post
+
+	err = json.Unmarshal(body, &post)
+
+	if err != nil {
+		presenters.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	post.Author.ID = userID
+
+	err = post.Validate()
+
+	if err != nil {
+		presenters.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := config.ConnectDatabase()
+
+	if err != nil {
+		presenters.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer db.Close()
+
+	repo := repositories.NewPostRepo(db)
+
+	postID, err := repo.CreatePost(post)
+
+	if err != nil {
+		presenters.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	post.ID = postID
+
+	presenters.JSON(w, http.StatusCreated, post)
+}
