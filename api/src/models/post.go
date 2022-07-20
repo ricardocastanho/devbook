88c42aee4d1384cf4780204b19889db5835c9a12
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2022-07-19 23:44:19 -0300

    ✨ add validate function in posts model

--- api/src/models/post.go
+++ api/src/models/post.go
@@ -1,12 +1,43 @@
 package models
 
+import (
+	"errors"
+	"strings"
+)
+
 type Post struct {
 	ID        string `json:"id,omitempty"`
 	Title     string `json:"title,omitempty"`
 	Content   string `json:"content,omitempty"`
 	Likes     uint64 `json:"likes"`
-	Author    string `json:"author,omitempty"`
+	Author    User   `json:"author,omitempty"`
 	CreatedAt string `json:"created_at,omitempty"`
 	UpdatedAt string `json:"updated_at,omitempty"`
 	DeletedAt string `json:"deleted_at,omitempty"`
+}
+
+func (p *Post) Validate() error {
+	err := p.checkEmptyFields()
+
+	if err != nil {
+		return err
+	}
+
+	p.trimFields()
+	return nil
+}
+
+func (p *Post) checkEmptyFields() error {
+	if p.Title == "" {
+		return errors.New("title is required")
+	}
+	if p.Content == "" {
+		return errors.New("content is required")
+	}
+	return nil
+}
+
+func (p *Post) trimFields() {
+	p.Title = strings.TrimSpace(p.Title)
+	p.Content = strings.TrimSpace(p.Content)
 }
